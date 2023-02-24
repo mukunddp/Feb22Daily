72560f91d05daeafdb23a2fdb4c26c8321ddd3dc
mukunddp <mukundparve98@gmail.com>
2023-02-24 09:05:07 +0530

    24-2 changed

---
+++ 4-String_methods.py
@@ -0,0 +1,29 @@
+# name = 'MUKUND'
+# institute = 'tech amplifiers'
+
+# syntax : string.method()
+# name.count('u')
+# print(name.count('u'))
+# print(name.count('n'))
+# print(name.lower())
+# print(name.title())
+# print(institute.title())
+# # print(institute.upper())
+#
+# institute = input('Type your institute name in uppercase')
+#
+# print(institute.isalnum())
+# print(institute.upper())
+# print(institute.translate('UTF-9'))
+name = 'MUKUND'
+institute = 'tech amplifiers'
+# print(len(name))
+
+# print(name.find('z'))
+# print(name.index('D'))
+# print(name.strip('MU'))
+# print(name.strip())
+# print(name.rstrip('D'))
+# print(name.lstrip('D'))
+print(institute.split())
+# print(institute.)
